0b67f5c77c0339245b5a34bc79279526c031b168
v.lozghachov <lvaldev.code@gmail.com>
2023-04-23 11:24:27 +0200

    add Linked lists | task 1

---
+++ LinkedLists/removeKFromList.js
@@ -0,0 +1,13 @@
+function solution(l, k) {
+  let temp = new ListNode();
+  temp.next = l;
+  let current = temp;
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return temp.next;
+}

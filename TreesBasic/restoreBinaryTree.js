689bbf9a6dfdc6e511ee0a7988f9c62d57c5d777
v.lozghachov <lvaldev.code@gmail.com>
2023-05-07 01:11:41 +0200

    add Trees basic #6 solution

--- TreesBasic/restoreBinaryTree.js
+++ TreesBasic/restoreBinaryTree.js
@@ -1,8 +1,19 @@
-//
-// Binary trees are already defined with this interface:
-// function Tree(x) {
-//   this.value = x;
-//   this.left = null;
-//   this.right = null;
-// }
-function solution(inorder, preorder) {}
+function restoreBinaryTree(inorder, preorder) {
+  const index = {};
+  inorder.forEach((val, idx) => (index[val] = idx));
+  let preIdx = 0;
+
+  function buildTreeHelper(start, end) {
+    if (start > end) return null;
+
+    const node = { value: preorder[preIdx++] };
+    const inIdx = index[node.value];
+
+    node.left = buildTreeHelper(start, inIdx - 1);
+    node.right = buildTreeHelper(inIdx + 1, end);
+
+    return node;
+  }
+
+  return buildTreeHelper(0, inorder.length - 1);
+}

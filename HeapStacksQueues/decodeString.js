c4c8e840ac974141bba291c99db66e3ed21af845
v.lozghachov <lvaldev.code@gmail.com>
2023-05-24 21:09:59 +0200

    add heap task 3 solution

--- HeapStacksQueues/decodeString.js
+++ HeapStacksQueues/decodeString.js
@@ -1,1 +1,22 @@
-function solution(s) {}
+function solution(s) {
+  const stack = [];
+  let currentStr = "";
+  let num = 0;
+
+  for (let char of s) {
+    if (!isNaN(char)) {
+      num = num * 10 + parseInt(char);
+    } else if (char === "[") {
+      stack.push([num, currentStr]);
+      num = 0;
+      currentStr = "";
+    } else if (char === "]") {
+      const [k, prevStr] = stack.pop();
+      currentStr = prevStr + currentStr.repeat(k);
+    } else {
+      currentStr += char;
+    }
+  }
+
+  return currentStr;
+}
